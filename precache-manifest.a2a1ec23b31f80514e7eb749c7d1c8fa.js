35975021a7199ccf4996309c33717408ecc76313
Nicholas Goudeau <ngoudeau2012@gmail.com>
2020-07-23 21:56:33 -0700

    Updates

---
+++ precache-manifest.a2a1ec23b31f80514e7eb749c7d1c8fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72879c07f34384ea6281d97f7622a44",
+    "url": "/employee_index_react/index.html"
+  },
+  {
+    "revision": "9c500a97d547225a4eb7",
+    "url": "/employee_index_react/static/css/main.6d64acd2.chunk.css"
+  },
+  {
+    "revision": "894a3665d7d5a5036b57",
+    "url": "/employee_index_react/static/js/2.13239055.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee_index_react/static/js/2.13239055.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c500a97d547225a4eb7",
+    "url": "/employee_index_react/static/js/main.796244d4.chunk.js"
+  },
+  {
+    "revision": "928103a602e2f1efa570",
+    "url": "/employee_index_react/static/js/runtime-main.ea78f94c.js"
+  }
+]);
